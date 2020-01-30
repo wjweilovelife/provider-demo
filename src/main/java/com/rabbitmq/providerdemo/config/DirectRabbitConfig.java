bf8ab7c21f9911d3b009e787b4dd1f9ffc5632ca
王建伟 <wjw@choicesoft.com.cn>
2020-01-30 22:08:24 +0800

    Initial commit

---
+++ src/main/java/com/rabbitmq/providerdemo/config/DirectRabbitConfig.java
@@ -0,0 +1,42 @@
+package com.rabbitmq.providerdemo.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DirectRabbitConfig {
+
+    //队列 起名：TestDirectQueue
+    @Bean
+    public Queue TestDirectQueue() {
+        return new Queue("TestDirectQueue",true);  //true 是否持久
+    }
+
+    //Direct交换机 起名：TestDirectExchange
+    @Bean
+    DirectExchange TestDirectExchange() {
+        return new DirectExchange("TestDirectExchange");
+    }
+
+    //绑定  将队列和交换机绑定, 并设置用于匹配键：TestDirectRouting
+    @Bean
+    Binding bindingDirect() {
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+
+
+    /**
+     * 消息推送到server，找到交换机了，但是没找到队列
+     * @return
+     */
+    @Bean
+    DirectExchange lonelyDirectExchange() {
+        return new DirectExchange("lonelyDirectExchange");
+    }
+
+
+}
